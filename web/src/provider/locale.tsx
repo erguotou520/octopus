ab475ab680b00fbcd96b1942bf5ffab52f2660a0
erguotou <erguotou525@gmail.com>
2026-03-17 07:59:58 +0800

    fix: fix locale

--- web/src/provider/locale.tsx
+++ web/src/provider/locale.tsx
@@ -4,21 +4,21 @@ import { type ReactNode } from 'react';
 import { NextIntlClientProvider } from 'next-intl';
 import { useSettingStore, type Locale } from '@/stores/setting';
 
-import zh_hansMessages from '../../public/locale/zh_hans.json';
-import zh_hantMessages from '../../public/locale/zh_hant.json';
+import zh_HansMessages from '../../public/locale/zh-Hans.json';
+import zh_HantMessages from '../../public/locale/zh-Hant.json';
 import enMessages from '../../public/locale/en.json';
 
-const messages: Record<Locale, typeof zh_hansMessages> = {
-    'zh-Hans': zh_hansMessages,
-    'zh-Hant': zh_hantMessages,
+const messages: Record<Locale, typeof zh_HansMessages> = {
+    'zh-Hans': zh_HansMessages,
+    'zh-Hant': zh_HantMessages,
     en: enMessages,
 };
 
 // Map internal locale codes to BCP 47 language tags
 // function toBCP47Locale(locale: Locale): string {
 //     const localeMap: Record<Locale, string> = {
-//         zh_hans: 'zh-Hans',
-//         zh_hant: 'zh-Hant',
+//         zh-Hans: 'zh-Hans',
+//         zh-Hant: 'zh-Hant',
 //         en: 'en',
 //     };
 //     return localeMap[locale];
